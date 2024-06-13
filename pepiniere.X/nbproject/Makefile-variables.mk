b3303036fc72b24f6ef4e69c81feb3c7fa526354
matheo-stephan <matheo.stephan@student.junia.com>
2024-06-13 14:09:44 +0200

    com

--- pepiniere.X/nbproject/Makefile-variables.mk
+++ pepiniere.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=AgriFlow.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/AgriFlow.production.hex
+CND_ARTIFACT_NAME_default=pepiniere.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/pepiniere.X.production.hex
