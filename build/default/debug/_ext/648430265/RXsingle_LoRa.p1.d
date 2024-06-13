6534f99d5f725035c0ca5a1fe60f08b3d07a8594
matheo-stephan <matheo.stephan@student.junia.com>
2024-06-13 13:37:08 +0200

    commit

---
+++ build/default/debug/_ext/648430265/RXsingle_LoRa.p1.d
@@ -0,0 +1,4 @@
+build/default/debug/_ext/648430265/RXsingle_LoRa.p1:  \
+../../../../Downloads/RXsingle_LoRa.X/RXsingle_LoRa.X/RXsingle_LoRa.c  \
+../../../../Downloads/RXsingle_LoRa.X/RXsingle_LoRa.X/general.h  \
+../../../../Downloads/RXsingle_LoRa.X/RXsingle_LoRa.X/uart.h 
---
+++ build/default/debug/_ext/648430265/uart.p1.d
@@ -0,0 +1,4 @@
+build/default/debug/_ext/648430265/uart.p1:  \
+../../../../Downloads/RXsingle_LoRa.X/RXsingle_LoRa.X/uart.c  \
+../../../../Downloads/RXsingle_LoRa.X/RXsingle_LoRa.X/general.h  \
+../../../../Downloads/RXsingle_LoRa.X/RXsingle_LoRa.X/uart.h 
